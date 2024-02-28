33fb11fc6bdbd23f32427c4ee1d8d881dc2da74c
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-27 20:05:14 -0600

    feat(providers/data/fetch-wrapper.ts): implement the complete customFetch function for the client provider

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,1 +1,15 @@
-const customFetch = async (url: string, options: RequestInit) => {};
+const customFetch = async (url: string, options: RequestInit) => {
+  const accessToken = localStorage.getItem("access_token");
+
+  const headers = options.headers as Record<string, string>;
+
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Preflight": "true",
+    },
+  });
+};
