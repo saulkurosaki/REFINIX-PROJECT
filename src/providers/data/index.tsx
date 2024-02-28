9d65f3aef1407ba6bb26842f18fc3d0e0fb19a49
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-27 20:32:59 -0600

    feat(providers/data/index.ts): implement the wsClient function

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,6 +1,10 @@
 import { GraphQLClient } from "@refinedev/nestjs-query";
+import { createClient } from "graphql-ws";
+import { fetchWrapper } from "./fetch-wrapper";
 
+export const API_BASE_URL = "https://api.crm.refine.dev";
 export const API_URL = "https://api.crm.refine.dev";
+export const WS_URL = "wss://api.crm.refine.dev/graphql";
 
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
@@ -11,3 +15,19 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+
+export const wsClient =
+  typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
