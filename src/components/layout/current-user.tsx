dfd674a441b7ab91adbe09a02f34373346b35246
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-29 19:57:53 -0600

    feat(CurrentUser): implement the account settings sidebar

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -6,6 +6,7 @@ import type { User } from '@/graphql/schema.types';
 import { Text } from '../text';
 import { SettingOutlined } from '@ant-design/icons';
 import { useState } from 'react';
+import { AccountSettings } from './account-settings';
 
 const CurrentUser = () => {
   
@@ -61,6 +62,14 @@ const CurrentUser = () => {
           style={{ cursor: 'pointer' }}
         />
       </Popover>
+
+      {user && (
+        <AccountSettings
+          opened={isOpen}
+          setOpened={setIsOpen}
+          userId={user.id}
+        />
+      )}
     </>
   )
 }
