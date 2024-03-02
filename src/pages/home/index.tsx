8d927ef2427e995c4da86e3389477b04f78ccc55
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-01 19:57:01 -0600

    feat(Home): implement the calendar upcoming events column

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,20 @@
+import { Col, Row } from "antd"
+
 export const Home = () => {
   return (
     <div>
-      Home Page test c:
+      <Row>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px',
+          }}
+        >
+          CalendarUpcomingEvents
+        </Col>
+      </Row>
     </div>
   )
 }
