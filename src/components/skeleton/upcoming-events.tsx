9a6f4c886fb33f73be56a68dfa3184403eec95eb
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-01 20:41:44 -0600

    feat(UpcomingEventsSkeleton): implement the complete UpcomingEventsSkeleton component

--- src/components/skeleton/upcoming-events.tsx
+++ src/components/skeleton/upcoming-events.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Badge, List, Skeleton } from "antd";
 
 const UpcomingEventsSkeleton = () => {
-  return <div>UpcomingEventsSkeleton skeleton</div>;
+  return (
+    <List.Item>
+      <List.Item.Meta
+        avatar={<Badge color="transparent" />}
+        title={
+          <Skeleton.Button
+            active
+            style={{
+              height: "14px",
+            }}
+          />
+        }
+        description={
+          <Skeleton.Button
+            active
+            style={{
+              width: "300px",
+              marginTop: "8px",
+              height: "16px",
+            }}
+          />
+        }
+      />
+    </List.Item>
+  );
 };
 
 export default UpcomingEventsSkeleton;
