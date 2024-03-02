c50558d1fe4744c1e9051bc4f1f252d8ff7920b7
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-01 20:43:45 -0600

    feat(LatestActivitiesSkeleton): implement the complete LatestActivitiesSkeleton component

--- src/components/skeleton/latest-activities.tsx
+++ src/components/skeleton/latest-activities.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { List, Skeleton } from "antd";
 
 const LatestActivitiesSkeleton = () => {
-  return <div>LatestActivitiesSkeleton skeleton</div>;
+  return (
+    <List.Item>
+      <List.Item.Meta
+        avatar={
+          <Skeleton.Avatar
+            active
+            size={48}
+            shape="square"
+            style={{
+              borderRadius: "4px",
+            }}
+          />
+        }
+        title={
+          <Skeleton.Button
+            active
+            style={{
+              height: "16px",
+            }}
+          />
+        }
+        description={
+          <Skeleton.Button
+            active
+            style={{
+              width: "300px",
+              height: "16px",
+            }}
+          />
+        }
+      />
+    </List.Item>
+  );
 };
 
 export default LatestActivitiesSkeleton;
