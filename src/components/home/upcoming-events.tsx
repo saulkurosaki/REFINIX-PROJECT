f192ba9cf33caa729be30341e9132c8978b6dcd3
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-01 20:24:11 -0600

    feat(UpcomingEvents): implement the complete Card component settings as well as the isLoading conditional rendering Lists component layout

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { CalendarOutlined } from "@ant-design/icons";
+import { Card, List } from "antd";
+import React, { useState } from "react";
+import { Text } from "../text";
 
 const UpcomingEvents = () => {
-  return (
-    <div>
-      UpcomingEvents component test c:
-    </div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
 
-export default UpcomingEvents
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            Upcoming Events
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+        ></List>
+      ) : (
+        <List></List>
+      )}
+    </Card>
+  );
+};
+
+export default UpcomingEvents;
