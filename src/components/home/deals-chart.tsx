a6e34303e2cebd39fb124362fec9b1ec2268a0b5
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 20:31:09 -0600

    feat(DealsChart): implement the dealData config data

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,11 +1,28 @@
+import { useMemo } from "react";
 import { DollarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import { Text } from "../text";
 import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/utilities/helpers";
 
 const DealsChart = () => {
+  const { data } = useList({
+    resource: "dealStages",
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+
+  const dealData = useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
   const config: AreaConfig = {
-    data: [],
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
   };
 
   return (
