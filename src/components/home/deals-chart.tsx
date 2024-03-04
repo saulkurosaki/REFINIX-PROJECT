6ec9086d12d8f252eb92d71493a2c22b2e414314
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 20:24:20 -0600

    feat(DealsChart): implement the ant design plots Area component within the DealsChart one

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,20 +1,35 @@
 import { DollarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
 
 const DealsChart = () => {
+  const config: AreaConfig = {
+    data: [],
+  };
+
   return (
     <Card
       style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "24px 24px 0 24px" }}
       title={
-        <div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
           <DollarOutlined />
           <Text size="sm" style={{ marginLeft: "0.5rem" }}>
             Deals
           </Text>
         </div>
       }
-    ></Card>
+    >
+      <Area {...config} height={325} />
+    </Card>
   );
 };
 
