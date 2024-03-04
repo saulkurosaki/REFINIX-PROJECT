f124aa1296bb4a703a3db3ed9565ed9a0fd306d0
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 20:45:00 -0600

    feat(Home): implement the dashboard total count card initial layout within the Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,18 @@ import { Col, Row } from "antd";
 export const Home = () => {
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+      </Row>
+
       <Row
         gutter={[32, 32]}
         style={{
