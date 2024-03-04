c70ff456bd91e365834a0be4de9aa0d34f1041f8
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 21:03:52 -0600

    feat(TotalCountCard): implement the component accepting props

--- src/components/home/total-count-card.tsx
+++ src/components/home/total-count-card.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+type Props = {
+  resource: "companies" | "contacts" | "deals";
+  isLoading: boolean;
+  totalCount: number;
+};
 
-const TotalCountCard = () => {
+const TotalCountCard = ({ resource, isLoading, totalCount }: Props) => {
   return <div>TotalCountCard Component c:</div>;
 };
 
