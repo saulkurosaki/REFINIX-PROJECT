cbfcd44b44c674f38197616401493d2e3c9f62e4
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 21:00:40 -0600

    feat(Home): implement the TotalCountCard components config props

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,41 @@
 import { DealsChart, TotalCountCard, UpcomingEvents } from "@/components";
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries";
+import { DashboardTotalCountsQuery } from "@/graphql/types";
+import { useCustom } from "@refinedev/core";
 import { Col, Row } from "antd";
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: "",
+    method: "get",
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  });
+
   return (
     <div>
       <Row gutter={[32, 32]}>
         <Col xs={24} sm={24} xl={8}>
-          <TotalCountCard />
+          <TotalCountCard
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-          <TotalCountCard />
+          <TotalCountCard
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
         </Col>
         <Col xs={24} sm={24} xl={8}>
-          <TotalCountCard />
+          <TotalCountCard
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
         </Col>
       </Row>
 
