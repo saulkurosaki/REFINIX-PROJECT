03779e6ad2cdd481acf76573b322cec858031d29
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 20:08:15 -0600

    feat(DealsChart): implement the DealsChart component card initial layout

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import { Text } from "../text";
 
 const DealsChart = () => {
   return (
-    <div>
-      DealsChart component test c:
-    </div>
-  )
-}
+    <Card
+      style={{ height: "100%" }}
+      title={
+        <div>
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    ></Card>
+  );
+};
 
-export default DealsChart
+export default DealsChart;
