b7a77ae4e93614d919c6713362d94898ab56798f
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-03 20:40:50 -0600

    feat(DealsChart): implement the complete DealsChart component

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -6,10 +6,19 @@ import { Area, AreaConfig } from "@ant-design/plots";
 import { useList } from "@refinedev/core";
 import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
 import { mapDealsData } from "@/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
-  const { data } = useList({
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
     resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
     meta: {
       gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
     },
@@ -23,6 +32,30 @@ const DealsChart = () => {
     data: dealData,
     xField: "timeText",
     yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 5,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        };
+      },
+    },
   };
 
   return (
