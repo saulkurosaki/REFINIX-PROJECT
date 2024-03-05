fa5bf9f44d2a1db8e60fc4de64f2bee85399ce59
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-04 23:56:55 -0600

    feat(TotalCountCard): implement the TotalCountCard complete component along as the complete chart config object

--- src/components/home/total-count-card.tsx
+++ src/components/home/total-count-card.tsx
@@ -1,6 +1,7 @@
 import { totalCountVariants } from "@/constants";
 import { Card, Skeleton } from "antd";
 import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
 
 type Props = {
   resource: "companies" | "contacts" | "deals";
@@ -11,6 +12,43 @@ type Props = {
 const TotalCountCard = ({ resource, isLoading, totalCount }: Props) => {
   const { primaryColor, secondaryColor, icon, title } =
     totalCountVariants[resource];
+
+  const config: AreaConfig = {
+    data: totalCountVariants[resource].data,
+    xField: "index",
+    yField: "value",
+    appendPadding: [1, 0, 0, 0],
+    padding: 0,
+    syncViewPadding: true,
+    autoFit: true,
+    tooltip: false,
+    animation: false,
+    xAxis: false,
+    yAxis: {
+      tickCount: 12,
+      label: {
+        style: {
+          stroke: "transparent",
+        },
+      },
+      grid: {
+        line: {
+          style: {
+            stroke: "transparent",
+          },
+        },
+      },
+    },
+    smooth: true,
+    line: {
+      color: primaryColor,
+    },
+    areaStyle: () => {
+      return {
+        fill: `l(270) 0:#fff 0.2${secondaryColor} 1:${primaryColor}`,
+      };
+    },
+  };
 
   return (
     <Card
@@ -56,6 +94,8 @@ const TotalCountCard = ({ resource, isLoading, totalCount }: Props) => {
             totalCount
           )}
         </Text>
+
+        <Area {...config} style={{ width: "50%" }} />
       </div>
     </Card>
   );
