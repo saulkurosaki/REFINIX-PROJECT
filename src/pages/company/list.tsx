5ecd66e9bffb9e83058f60aebd763a4d561f5472
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-05 19:51:49 -0600

    feat(CompanyList): implement the company list table initial layout along as the pagination

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
+import { CreateButton, List, useTable } from "@refinedev/antd";
+import { useGo } from "@refinedev/core";
+import { Table } from "antd";
 
 export const CompanyList = () => {
-  return <div>CompanyList page test c:</div>;
+  const go = useGo();
+
+  const { tableProps, filters } = useTable({
+    resource: "companies",
+    pagination: {
+      pageSize: 12,
+    },
+    meta: {
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
+
+  return (
+    <List
+      breadcrumb={false}
+      headerButtons={() => (
+        <CreateButton
+          onClick={() => {
+            go({
+              to: {
+                resource: "companies",
+                action: "create",
+              },
+              options: {
+                keepQuery: true,
+              },
+              type: "replace",
+            });
+          }}
+        />
+      )}
+    >
+      <Table
+        {...tableProps}
+        pagination={{
+          ...tableProps.pagination,
+        }}
+      ></Table>
+    </List>
+  );
 };
