b938e1e99ee55523f92da25cf602c92384590ce6
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-05 18:21:05 -0600

    feat(LatestActivities): implement the latest activities card initial layout

--- src/components/home/latest-activities.tsx
+++ src/components/home/latest-activities.tsx
@@ -1,7 +1,24 @@
+import { UnorderedListOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import { title } from "process";
 import React from "react";
+import { Text } from "../text";
 
 const LatestActivities = () => {
-  return <div>LatestActivities component Test c:</div>;
+  return (
+    <Card
+      headStyle={{ padding: "16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <UnorderedListOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            LatestActivities
+          </Text>
+        </div>
+      }
+    ></Card>
+  );
 };
 
 export default LatestActivities;
