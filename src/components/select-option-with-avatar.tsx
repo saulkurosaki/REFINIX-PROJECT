87cc0a89417618e5807a755ab2548ae0eeb56020
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-07 19:00:36 -0600

    feat(SelectOptionWithAvatar): implement the SelectOptionWithAvatar component initial layout

---
+++ src/components/select-option-with-avatar.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  name: string;
+  avatarUrl?: string;
+  shape?: "circle" | "square";
+};
+
+const SelectOptionWithAvatar = ({ name, avatarUrl, shape }: Props) => {
+  return <div>SelectOptionWithAvatar component Test c:</div>;
+};
+
+export default SelectOptionWithAvatar;
