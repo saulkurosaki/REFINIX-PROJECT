77ef4dcb2f237fae407c0927fdf9474940447402
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-07 18:12:20 -0600

    feat(App.tsx): implement the CreateCompany component into a new route in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,14 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
+import {
+  Home,
+  ForgotPassword,
+  Login,
+  Register,
+  CompanyList,
+  CreateCompany,
+} from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
@@ -62,7 +69,10 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
-                  <Route path="/companies" element={<CompanyList />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<CreateCompany />} />
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
