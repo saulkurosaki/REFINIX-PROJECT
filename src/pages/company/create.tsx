e6b61f6c923f58e334ac4d5bb62bdde51c260066
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-07 18:19:30 -0600

    feat(CreateCompany): implement all of the useModalForm config options object

--- src/pages/company/create.tsx
+++ src/pages/company/create.tsx
@@ -1,3 +1,35 @@
+import { Modal } from "antd";
+import { CompanyList } from "./list";
+import { useModalForm } from "@refinedev/antd";
+import { useGo } from "@refinedev/core";
+import { CREATE_COMPANY_MUTATION } from "@/graphql/mutations";
+
 export const CreateCompany = () => {
-  return <div>CreateCompany component Test</div>;
+  const go = useGo();
+
+  // const goToListPage = () => {
+  //   go({
+  //     to: { resource: 'companies', action: 'list' },
+  //     options: { keepQuery: true },
+  //     type: 'replace',
+  //   });
+  // };
+
+  const { formProps, modalProps } = useModalForm({
+    action: "create",
+    defaultVisible: true,
+    resource: "companies",
+    redirect: false,
+    mutationMode: "pessimistic",
+    onMutationSuccess: goToListPage,
+    meta: {
+      gqlMutation: CREATE_COMPANY_MUTATION,
+    },
+  });
+
+  return (
+    <CompanyList>
+      <Modal></Modal>
+    </CompanyList>
+  );
 };
