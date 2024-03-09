9ab3899432d62707a21246c93874d66f1af3202c
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-09 11:38:15 -0600

    feat(EditPage): implement the initial edit page form structure along as the editing company custom avatar

--- src/pages/company/edit.tsx
+++ src/pages/company/edit.tsx
@@ -1,3 +1,39 @@
-export const Edit = () => {
-  return <div>Edit page component Test c:</div>;
+import { Col, Form, Row } from "antd";
+import { Edit, useForm } from "@refinedev/antd";
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import CustomAvatar from "@/components/custom-avatar";
+import { getNameInitials } from "@/utilities";
+
+export const EditPage = () => {
+  const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+    redirect: false,
+    meta: {
+      gqlMutation: UPDATE_COMPANY_MUTATION,
+    },
+  });
+
+  const { avatarUrl, name } = queryResult?.data?.data || {};
+
+  return (
+    <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Edit
+            isLoading={formLoading}
+            saveButtonProps={saveButtonProps}
+            breadcrumb={false}
+          >
+            <Form {...formProps} layout="vertical">
+              <CustomAvatar
+                shape="square"
+                src={avatarUrl}
+                name={getNameInitials(name || "")}
+                style={{ width: 96, height: 96, marginBottom: "24px" }}
+              />
+            </Form>
+          </Edit>
+        </Col>
+      </Row>
+    </div>
+  );
 };
