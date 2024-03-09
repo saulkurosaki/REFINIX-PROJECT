4c83ba0364668034f726fd13a37aabea2dc9d069
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-09 13:32:01 -0600

    feat(KanbanColumn): implement the component intitial styling and structure with the antd spaces components

--- src/components/tasks/kanban/column.tsx
+++ src/components/tasks/kanban/column.tsx
@@ -1,5 +1,35 @@
+import { Text } from "@/components/text";
+import { useDroppable } from "@dnd-kit/core";
+import { Space } from "antd";
+
 const KanbanColumn = () => {
-  return <div>KanbanColumn component Test c:</div>;
+  const { isOver, setNodeRef, active } = useDroppable({
+    id: "",
+    data: "",
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "0 16px",
+      }}
+    >
+      <div
+        style={{
+          padding: "12px",
+        }}
+      >
+        <Space style={{ width: "100%", justifyContent: "space-between" }}>
+          <Space>
+            <Text>TITLE TO DO</Text>
+          </Space>
+        </Space>
+      </div>
+    </div>
+  );
 };
 
 export default KanbanColumn;
