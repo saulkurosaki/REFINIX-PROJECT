2c359c6bd64adc71164869a5680f3574c3bbb919
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-09 12:23:48 -0600

    feat(KanbanBoardContainer): implement the KanbanBoardContainer component initial layout

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -8,6 +8,7 @@ import KanbanColumnSkeleton from "./skeleton/kanban";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import ContactStatusTag from "./tags/contact-status-tag";
+import KanbanBoardContainer from "./tasks/kanban/board";
 
 export {
   UpcomingEvents,
@@ -20,4 +21,5 @@ export {
   LatestActivitiesSkeleton,
   ProjectCardSkeleton,
   ContactStatusTag,
+  KanbanBoardContainer,
 };
---
+++ src/components/tasks/kanban/board.tsx
@@ -0,0 +1,5 @@
+const KanbanBoardContainer = () => {
+  return <div>KanbanBoardContainer component Test c:</div>;
+};
+
+export default KanbanBoardContainer;
