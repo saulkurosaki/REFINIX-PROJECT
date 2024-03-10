02534db3d64020dd1e77cece4553948ab377231b
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-10 11:50:56 -0600

    feat(KanbanItem): implement the useDraggable hook config object options

--- src/components/tasks/kanban/item.tsx
+++ src/components/tasks/kanban/item.tsx
@@ -1,4 +1,11 @@
+import { useDraggable } from "@dnd-kit/core";
+
 const KanbanItem = () => {
+  const { attributes, listeners, setNodeRed, active } = useDraggable({
+    id: "",
+    data: "data",
+  });
+
   return <div>Kanban Item component Test c:</div>;
 };
 
