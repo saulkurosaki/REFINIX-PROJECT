e721c11260f244e891cb9ea29d1877688335ee32
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-10 12:07:10 -0600

    feat(TasksList): implement the useList hook config object options to fetch the tasks data

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -4,8 +4,26 @@ import {
   KanbanColumn,
   KanbanItem,
 } from "@/components";
+import { TASKS_QUERY } from "@/graphql/queries";
+import { useList } from "@refinedev/core";
 
 export const TasksList = () => {
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+    resource: "tasks",
+    sorters: [
+      {
+        field: "dueDate",
+        order: "asc",
+      },
+    ],
+    pagination: {
+      mode: "off",
+    },
+    meta: {
+      gqlQuery: TASKS_QUERY,
+    },
+  });
+
   return (
     <>
       <KanbanBoardContainer>
