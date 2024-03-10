37cff85fbae673dd5f037e2a193b1b4b89ffd19d
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-10 15:39:01 -0600

    feat(ProjectCard): implement the ProjectCard component props type

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -1,4 +1,18 @@
-const ProjectCard = () => {
+import { User } from "@/graphql/schema.types";
+
+type ProjectCardProps = {
+  id: string;
+  title: string;
+  updatedAt: string;
+  dueDate?: string;
+  users?: {
+    id: string;
+    name: string;
+    avatarUrl?: User["avatarUrl"];
+  }[];
+};
+
+const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
   return <div>ProjectCard component Test c:</div>;
 };
 
