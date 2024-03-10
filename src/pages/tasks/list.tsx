0f5a1dd66858d209e2711e3fb7f15dcd9ef6326c
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-10 12:50:55 -0600

    feat(TasksList): implement the useList hook config object options to fetch the tasks stages

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -4,10 +4,30 @@ import {
   KanbanColumn,
   KanbanItem,
 } from "@/components";
-import { TASKS_QUERY } from "@/graphql/queries";
+import { TASKS_QUERY, TASK_STAGES_QUERY } from "@/graphql/queries";
 import { useList } from "@refinedev/core";
 
 export const TasksList = () => {
+  const { data: stages, isLoading: isLoadingStages } = useList({
+    resource: "taskStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["TODO", "IN", "IN PROGRESS", "DONE"],
+      },
+    ],
+    sorters: [
+      {
+        field: "createdAt",
+        order: "asc",
+      },
+    ],
+    meta: {
+      gqlQuery: TASK_STAGES_QUERY,
+    },
+  });
+
   const { data: tasks, isLoading: isLoadingTasks } = useList({
     resource: "tasks",
     sorters: [
