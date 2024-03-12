a308498ad4aa7c1056183ba0ecd47b92dfec26df
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-11 19:13:09 -0600

    feat(ProjectCard): implement the complete ProjectCard component

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -146,9 +146,20 @@ const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
           )}
 
           {!!users?.length && (
-            <Space>
+            <Space
+              size={4}
+              wrap
+              direction="horizontal"
+              align="center"
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginLeft: "auto",
+                marginRight: 0,
+              }}
+            >
               {users.map((user) => (
-                <Tooltip>
+                <Tooltip key={user.id} title={user.name}>
                   <CustomAvatar name={user.name} src={user.avatarUrl} />
                 </Tooltip>
               ))}
