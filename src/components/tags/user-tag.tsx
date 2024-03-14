c84a406c0a256405d87cb679437873a2da67dc8c
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-13 20:03:46 -0600

    feat(UserTag): implement the complete UserTag component

--- src/components/tags/user-tag.tsx
+++ src/components/tags/user-tag.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Space, Tag } from "antd";
 
-const UserTag = () => {
-  return <div>User Tag component</div>;
+import { User } from "@/graphql/schema.types";
+import CustomAvatar from "../custom-avatar";
+
+type Props = {
+  user: User;
+};
+
+const UserTag = ({ user }: Props) => {
+  return (
+    <Tag
+      key={user.id}
+      style={{
+        padding: 2,
+        paddingRight: 8,
+        borderRadius: 24,
+        lineHeight: "unset",
+        marginRight: "unset",
+      }}
+    >
+      <Space size={4}>
+        <CustomAvatar
+          src={user.avatarUrl}
+          name={user.name}
+          style={{ display: "inline-flex" }}
+        />
+        {user.name}
+      </Space>
+    </Tag>
+  );
 };
 
 export default UserTag;
