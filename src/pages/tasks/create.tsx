c7b356a2939e8df76e09093d7bcd95792530ac5a
Saul Paredes <saulkurosaki13@gmail.com>
2024-03-13 19:06:09 -0600

    feat(CreateTask): implement the modal component config props

--- src/pages/tasks/create.tsx
+++ src/pages/tasks/create.tsx
@@ -21,34 +21,33 @@ export const CreateTask = () => {
   });
 
   return (
-    <div></div>
-    // <Modal
-    //   {...modalProps}
-    //   onCancel={() => {
-    //     close();
+    <Modal
+      {...modalProps}
+      onCancel={() => {
+        close();
 
-    //     list("tasks", "replace");
-    //   }}
-    //   title="Add new card"
-    //   width={512}
-    // >
-    //   <Form
-    //     {...formProps}
-    //     layout="vertical"
-    //     onFinish={(values) => {
-    //       formProps?.onFinish?.({
-    //         ...values,
-    //         stageId: searchParams.get("stageId")
-    //           ? Number(searchParams.get("stageId"))
-    //           : null,
-    //         userIds: [],
-    //       });
-    //     }}
-    //   >
-    //     <Form.Item label="Title" name="title" rules={[{ required: true }]}>
-    //       <Input />
-    //     </Form.Item>
-    //   </Form>
-    // </Modal>
+        list("tasks", "replace");
+      }}
+      title="Add new card"
+      width={512}
+    >
+      {/* <Form
+        {...formProps}
+        layout="vertical"
+        onFinish={(values) => {
+          formProps?.onFinish?.({
+            ...values,
+            stageId: searchParams.get("stageId")
+              ? Number(searchParams.get("stageId"))
+              : null,
+            userIds: [],
+          });
+        }}
+      >
+        <Form.Item label="Title" name="title" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+      </Form> */}
+    </Modal>
   );
 };
